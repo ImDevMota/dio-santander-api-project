fb9cf7b86a59a6f41bafd4c274ab8539df4cbc97
thiago mota <thiago.you23@gmail.com>
2023-10-21 20:09:07 -0300

    Adicionando configuracoes iniciais

---
+++ src/test/java/my/project/dio/SantanderApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
